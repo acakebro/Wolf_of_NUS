3239e41b671871829922521a9c92b54b8a174eb7
757cyh <67052883+acakebro@users.noreply.github.com>
2022-10-09 10:52:06 +0800

    saturday's work

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,4 +1,61 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API } = process.env;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+    overrides: {
+      "contracts/WulfzContracts/Wulfz.sol": {
+        version: "0.8.10",
+        settings: {}
+      },
+      "contracts/WulfzContracts/StakingPool.sol": {
+        version: "0.8.10",
+        settings: {}
+      },
+      "contracts/WulfzContracts/Profile.sol": {
+        version: "0.8.10",
+        settings: {}
+      },
+      "contracts/WulfzContracts/WulfzAwoo.sol": {
+        version: "0.8.10",
+        settings: {}
+      },
+    }
+  },
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API
+  }
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import {
-  chain,
-  configureChains,
-  createClient,
-  WagmiConfig,
-} from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-const { chains, provider } = configureChains(
-  [chain.goerli],
-  [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
-);
 
-const { connectors } = getDefaultWallets({
-  appName: 'Mash',
-  chains
-});
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider
-})
 
 function App() {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <ConnectButton />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <ConnectButton showBalance={false} />
   );
 }
 
